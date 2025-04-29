c10b3f99cedd2924176321293054273845ba05f7
nihei.seiji <nihei.seiji.office@gmail.com>
2025-04-29 21:50:08 +0900

    [fix]spa構成への変更

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import { sveltePreprocess } from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({ out: 'build' })
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    })
   },
   preprocess: sveltePreprocess({
     postcss: true
